8f3807729fc4212b4e723eb1593ff3105f2c7342
Arthur Hermann <arthurhermannc@gmail.com>
2021-11-01 15:32:00 -0300

    feat(register): add middlewares verications to register

---
+++ back-end/src/middlewares/index.js
@@ -0,0 +1,5 @@
+const verifyDataToRegister = require('./verifyDataToRegister');
+
+module.exports = {
+  verifyDataToRegister,
+}
---
+++ back-end/src/middlewares/verifyDataToRegister.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+const { UNPROCESSABLE_ENTITY } = require('http-status');
+
+module.exports = (req, res, next) => {
+
+  const data = req.body;
+
+  const { error } = Joi.object({
+    email: Joi.string().email().not().empty()
+    .required(),
+    password:  Joi.string().min(6).pattern(/^(?=.*[a-zA-Z])(?=.*[0-9])/).not().empty()
+    .required(),
+    nome: Joi.string().not().empty()
+    .required(),
+
+  }).validate(data);
+
+  if (error) {
+    return res.status(UNPROCESSABLE_ENTITY).json({ message: error.details[0].message });
+  };
+
+  next();
+};
