21b4208de62c018f614283c5be7eafac9e2e0ca7
Arthur Hermann <arthurhermannc@gmail.com>
2021-11-02 12:37:39 -0300

    feat(tasklistservice): add update

--- back-end/src/services/taskListService.js
+++ back-end/src/services/taskListService.js
@@ -1,4 +1,6 @@
 const taskListModel = require('../model/taskListModel');
+const { onlyCreatorEdit } = require('../helpers/errors');
+const { isSameId } = require('../validations/index');
 
 const create = async (listData) => {
   const response = await taskListModel.create(listData);
@@ -6,6 +8,23 @@ const create = async (listData) => {
   return response;
 };
 
+const update = async (userData, taskListId, tasksListData) => {
+  const { _id } = userData;
+  const userId = _id;
+
+  const taskListToEdit = await taskListModel.findTaskListById(taskListId);
+  const createdBy = taskListToEdit.author;
+  
+  if (!isSameId(userId, createdBy)) {
+    return { err: { message: onlyCreatorEdit } };
+  }
+
+  await recipesModel.update(taskListId, tasksListData);
+
+  return { ...taskListToEdit, ...tasksListData };
+};
+
 module.exports = {
   create,
+  update,
 };
