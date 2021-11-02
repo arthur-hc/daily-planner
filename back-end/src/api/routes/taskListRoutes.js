3a17c1f6bc27f3973906761f803bf894ab47df23
Arthur Hermann <arthurhermannc@gmail.com>
2021-11-02 12:50:03 -0300

    feat(verifyidformat): add id format verification to middlewares

--- back-end/src/api/routes/taskListRoutes.js
+++ back-end/src/api/routes/taskListRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const validationJWT = require('../auth/validationJWT');
 const taskListController = require('../../controllers/taskListController');
-const { verifyTaskListData } = require('../../middlewares');
+const { verifyTaskListData, verifyIdFormat } = require('../../middlewares');
 
 const router = express.Router();
 
 router.post('/', validationJWT, verifyTaskListData, taskListController.create);
 
-router.put('/:id', validationJWT, verifyTaskListData, taskListController.update);
+router.put('/:id', verifyIdFormat, validationJWT, verifyTaskListData, taskListController.update);
 
 module.exports = router;
