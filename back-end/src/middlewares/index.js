6ea11db0b1d1b953d508042026f157b324fa4b1b
Arthur Hermann <arthurhermannc@gmail.com>
2021-11-02 14:55:50 -0300

    feat(verifytasklistname): add middlware to validate task list name before create

--- back-end/src/middlewares/index.js
+++ back-end/src/middlewares/index.js
@@ -2,10 +2,12 @@ const verifyDataToRegister = require('./verifyDataToRegister');
 const verifyDataToLogin = require('./verifyDataToLogin');
 const verifyTaskListData = require('./verifyTaskListData');
 const verifyIdFormat = require('./verifyIdFormat');
+const verifyTaskListName = require('./verifyTaskListName');
 
 module.exports = {
   verifyDataToRegister,
   verifyDataToLogin,
   verifyTaskListData,
   verifyIdFormat,
+  verifyTaskListName,
 };
---
+++ back-end/src/middlewares/verifyTaskListName.js
@@ -0,0 +1,17 @@
+const Joi = require('joi');
+const { BAD_REQUEST } = require('http-status');
+
+module.exports = (req, res, next) => {
+  const taskListData = req.body;
+
+  const { error } = Joi.object({
+    TaskListName: Joi.string().not().empty()
+    .required(),
+  }).validate(taskListData);
+
+  if (error) {
+    return res.status(BAD_REQUEST).json({ error: { message: error.details[0].message } });
+  }
+
+  next();
+};
