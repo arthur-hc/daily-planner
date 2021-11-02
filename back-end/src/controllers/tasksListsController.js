37b24fc91f8fc08e551db7080614f74dc8fe193b
Arthur Hermann <arthurhermannc@gmail.com>
2021-11-01 21:14:44 -0300

    feat(taskslistscontroller): add create

--- back-end/src/controllers/tasksListsController.js
+++ back-end/src/controllers/tasksListsController.js
@@ -3,8 +3,8 @@ const tasksListsService = require('../services/tasksListsService');
 
 const create = async (req, res) => {
   const { _id } = req.userData;
-  const listData = req.body;
-  const response = await tasksListsService.create({ author: _id, date: listData });
+  const tasks = req.body;
+  const response = await tasksListsService.create({ author: _id, tasks});
 
   return res.status(CREATED).json(response);
 };
