58e49d310f435bb6ef93761536b2de9b852cc25d
Arthur Hermann <arthurhermannc@gmail.com>
2021-11-04 20:49:21 -0300

    feat(mylistpage): change box size css

--- front-end/daily-planner/src/components/ListCard.js
+++ front-end/daily-planner/src/components/ListCard.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Card, Button } from 'react-bootstrap';
+import { Card, Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import taskListImg from '../img/tasklist.png';
 
 const ListCard = ({ title, id }) => (
-  <Link to={ `/mylists/${id}` }>
-    <Card style={ { width: '10rem', height: '15rem' } }>
-      <Card.Img variant="top" src={ taskListImg } style={ { width: '3rem' } } />
-      <Card.Body>
+
+  <Card style={ { width: '10rem', height: '15rem' } }>
+    <Card.Body className="d-flex-column">
+      <Container className="h-25">
+        <Card.Img variant="top" src={ taskListImg } style={ { width: '3rem' } } />
+      </Container>
+      <Container className="h-50">
         <Card.Title>{title}</Card.Title>
-        <Button variant="primary">See Task List</Button>
-      </Card.Body>
-    </Card>
-  </Link>
+      </Container>
+      <Container className="h-25">
+        <Link to={ `/mylists/${id}` }>
+          <Button variant="primary">See Task List</Button>
+        </Link>
+      </Container>
+    </Card.Body>
+  </Card>
+
 );
 
 ListCard.propTypes = {
