010b35b4ae86ba331693951cfcbed23ab8ed110f
Arthur Hermann <arthurhermannc@gmail.com>
2021-11-04 10:22:45 -0300

    feat(initialmenu): change page name from login to initial page

--- front-end/daily-planner/src/pages/InitialPage.js
+++ front-end/daily-planner/src/pages/InitialPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import LoginForms from '../components/LoginForms';
 
-function Login() {
+function InitialPage() {
   return (
     <Container
       className="d-flex justify-content-center align-items-center vh-100 vw-100"
@@ -12,4 +12,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default InitialPage;
