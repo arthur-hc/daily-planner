3ba9f3908fd8b9cd0deffb46628a0e1809a6334b
Arthur Hermann <arthurhermannc@gmail.com>
2021-11-03 21:45:55 -0300

    feat(login): add login forms missing some fetch to api

---
+++ front-end/daily-planner/src/components/LoginForms.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+
+function LoginForms() {
+  const [invalidEntries, setInvalidEntries] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLoginClick = (event) => {
+    event.preventDefault();
+    if (!email.includes('arthur')) {
+      setInvalidEntries(true);
+    }
+  };
+
+  const alert = () => {
+    if (invalidEntries) {
+      return (
+        <Alert
+          className="m-1"
+          variant="danger"
+          onClick={ () => setInvalidEntries(false) }
+        >
+          Incorrect Email or password
+        </Alert>
+      );
+    }
+  };
+
+  return (
+    <Form
+      className="h-50 p-3 border border-1 border-dark rounded"
+      onSubmit={ handleLoginClick }
+    >
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control
+          type="email"
+          placeholder="Enter email"
+          value={ email }
+          required
+          onChange={ ({ target: { value } }) => setEmail(value) }
+        />
+        <Form.Control.Feedback type="invalid">
+          Please provide a valid Email.
+        </Form.Control.Feedback>
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          required
+          type="password"
+          placeholder="Password"
+          value={ password }
+          onChange={ ({ target: { value } }) => setPassword(value) }
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+      {alert()}
+    </Form>
+  );
+}
+
+export default LoginForms;
